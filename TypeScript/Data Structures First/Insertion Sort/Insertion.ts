c748b192f7dd3e80ef94bb01ac188e1852df1aa6
monarie <jobmonari7@gmail.com>
2023-01-23 17:51:34 +0300

    Insertion Sort

--- TypeScript/Data Structures First/Insertion Sort/Insertion.ts
+++ TypeScript/Data Structures First/Insertion Sort/Insertion.ts
@@ -1,5 +1,5 @@
 export const insertionSort = (arr: number[]): number[] => {
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     let j = i - 1
     while (j >= 0 && arr[j + 1] < arr[j]) {
       let tmp = arr[j + 1]
